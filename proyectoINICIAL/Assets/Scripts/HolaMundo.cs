6ee125347c9d6ac43a93c0c9018dd5069a6692de
chacafdz <chacafdz@gmail.com>
2026-03-03 20:29:50 -0600

    SE CAMBIO EL GITNORE
    CAMBIO DE CODIGO DE GITNORE

---
+++ proyectoINICIAL/Assets/Scripts/HolaMundo.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HolaMundo : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("hola mundo");
+        Debug.LogWarning("algo salio mal");
+        Debug.LogError("algo salio muy mal");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
