27ae3d781ba05be70da58f6fff309ce23a749266
chacafdz <chacafdz@gmail.com>
2026-03-10 12:10:08 -0600

    orden de ejecucion
    ase crearon 3 scripts, para poder ver el orden de ejucucion y poder ver como cambiarlo.

--- proyectoINICIAL/Assets/Scripts/HolaMundo.cs
+++ proyectoINICIAL/Assets/Scripts/HolaMundo.cs
@@ -9,7 +9,7 @@ public class HolaMundo : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        x - 0;
+        x = 0;
         print("Algo paso");
         Debug.LogWarning("Algo salio medianamente mal");
         Debug.LogError("Algo salio muy mal");
@@ -18,7 +18,24 @@ public class HolaMundo : MonoBehaviour
     // Update is called once per frame
     void Update() // <-- Update should be OUTSIDE of Start
     {
-        x = x + 1;
-        Debug.Log("Algo paso");
+        //x += 1;
+        //Debug.Log(x);
+        Debug.Log("hola desde update");
+    }
+    private void FixedUpdate()
+    {
+        Debug.Log("hola desde fixed Update cada 50 frames");
+    }
+    private void LateUpdate()
+    {
+        Debug.Log(" hola desde Late Uodate ");
+    }
+    private void OnEnable()
+    {
+        Debug.Log("EL objeto ha sido hablitado ");
+    }
+    private void OnDisable()
+    {
+        Debug.Log("el objeto ha sido inhabilitado");
     }
 }
