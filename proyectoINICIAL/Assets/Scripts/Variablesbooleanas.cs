6b038a8d5e31bfb440bfbcd4d7c3eea5a9c84318
chacafdz <chacafdz@gmail.com>
2026-05-19 17:36:31 -0600

    cambio de color en los go.
    cambio de color en los go, creacion de variables booleanas

---
+++ proyectoINICIAL/Assets/Scripts/Variablesbooleanas.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Variablesbooleanas : MonoBehaviour
+{
+    bool variable1;
+    bool variable2;
+    bool variable3;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(variable1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
