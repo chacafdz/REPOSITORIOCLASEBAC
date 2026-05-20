eef35acce87d2ee5912580e3928e15770b56f11a
chacafdz <chacafdz@gmail.com>
2026-05-19 18:31:45 -0600

    variables booleanas
    Se crearon 5 scripts, con variables booleanas, utilizado OR, AND y se conbinaron

---
+++ proyectoINICIAL/Assets/Scripts/CuboBooleano1.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class CuboBooleano1 : MonoBehaviour
+{
+    public bool valor = false;
+
+    void FixedUpdate()
+    {
+        valor = !valor;
+
+        Renderer rend = GetComponent<Renderer>();
+        rend.material.color = valor ? Color.white : Color.black;
+
+        Debug.Log("GO1 valor: " + valor);
+    }
+}
