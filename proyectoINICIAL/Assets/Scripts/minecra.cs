2e81b6bd4613f184b6480ac015a8344030bdba89
chacafdz <chacafdz@gmail.com>
2026-08-05 22:26:25 -0600

    g
    g

--- proyectoINICIAL/Assets/Scripts/minecra.cs
+++ proyectoINICIAL/Assets/Scripts/minecra.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+//dgfdgfdgdfg
 public class minecra : MonoBehaviour
 {
     // Start is called before the first frame update
